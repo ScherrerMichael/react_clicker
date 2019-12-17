00ae1cd47b5777959316b42b534d2958c909280a
chris <chrisrhayden@gmail.com>
2019-12-17 15:52:17 -0800

    add info module

---
+++ src/components/Info.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export interface InfoProps { user_score: number, click_power: number };
+
+/** basic info
+ *
+ * in javascript anonymous functions will return if there is only one thing
+ * javascript is NOT white space dependent so a new line dose not mater
+ * its there for readability
+ *
+ * this can be refactored into a class when we want more things
+ */
+
+export const Info = (props: InfoProps) =>
+  <p>you have {props.user_score} with {props.click_power} click power</p>;
