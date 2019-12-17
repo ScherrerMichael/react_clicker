7f1fe2bb2b3860c148f12e05a07fab8d64ed6908
chris <chrisrhayden@gmail.com>
2019-12-16 19:26:40 -0800

    adding comments

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 // interface's are what the typescript compiler uses to check objects
 export interface GameProps { compiler: string; framework: string; }
 
-// a function that returns a react h1
+// a function that returns a react h1 using the passed in data
 export const Game = (props: GameProps) => {
     return <h1>using {props.compiler} with the {props.framework} framework </h1>;
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
+// the typescript index file
+
+// our project, the Game.tsx module essentially, will be called from here
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import { Game } from './components/Game';
 
+// ask react to render the game to the empty div in ./index.html
 ReactDOM.render(
+  // send the Game component data called; compiler and framework
   <Game compiler='TypeScript' framework='React' />,
   document.getElementById('main-game')
 );
